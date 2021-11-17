bd033a624104e28916df43a1a4d9a61b2672972b
Anthony Bigotte <bigotte.anthony@gmail.com>
2021-11-17 15:45:49 +0100

    à faire : gestion des droits d'admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -82,6 +82,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles;
+
 
     public function __construct()
     {
@@ -258,7 +263,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
     }
 
     public function getSalt()
